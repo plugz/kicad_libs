1b8b439d15a5aad6cfb37e2ee6e8671f8c675c37
plugz <david.garyga@gmail.com>
2026-02-15 13:31:27 +0100

    remove init.sh

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://layouteditor.com/releases/20200104/layout-20200104-Linux.x86_64.tar.bz2
-tar xf layout-20200104-Linux.x86_64.tar.bz2
-ln -s layout/bin/freeRouting.jar freeRouting.jar
-
-echo check changes to do in kicad_conf.txt
